787aa375c350b4fc8d596e7470e7a207f728db0f
Diego Chavarria <diego.chavarriap@gmail.com>
2020-10-07 15:51:35 -0500

    Updates

---
+++ precache-manifest.51fdf79f9a01dcf0a5c744e44ea27523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365962ad19514fa2e40dbff1063d64eb",
+    "url": "/People-analytics-frontend/index.html"
+  },
+  {
+    "revision": "d8e7abd412a07e8284af",
+    "url": "/People-analytics-frontend/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "d8e7abd412a07e8284af",
+    "url": "/People-analytics-frontend/static/js/2.0317f199.chunk.js"
+  },
+  {
+    "revision": "6553848ede33e774934d8afdf700acf9",
+    "url": "/People-analytics-frontend/static/js/2.0317f199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc18fd53e4af508f973",
+    "url": "/People-analytics-frontend/static/js/main.eb7687b1.chunk.js"
+  },
+  {
+    "revision": "520a67aacfc025a531a4",
+    "url": "/People-analytics-frontend/static/js/runtime-main.0d156487.js"
+  }
+]);
